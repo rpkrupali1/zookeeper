91587d3fe7128e85e66649629b7770116eba4609
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-10 19:09:13 -0600

    final updates

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-//front end files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.listen(3001,()=>{
-    console.log('Api server is now on port 3001');
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
